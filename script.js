0797d5c4636527d89b7c5979f283f5abf2505d31
BruLekichLouro <bububio@yahoo.com.br>
2022-08-10 11:23:25 -0300

    Finalizando o projeto do palindromo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function verificaPalindromo(string){
-    
+    if(!string) return "String inexistente";
+
+    for(let i = 0; i < string.length / 2; i++){ //divido por 2 para verificar 2 caracteres ao mesmo tempo
+        if(string[i] !== string[string.length - 1 -i]){
+            var texto = "Não é um palíndromo"
+            return false;
+            
+        }
+    }
+    return true;
 }
+console.log(verificaPalindromo("abba"));
+console.log(verificaPalindromo("ola"));
