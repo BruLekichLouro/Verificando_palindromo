f1115ea0a15d606f7902722c283f7af0b08f8229
BruLekichLouro <bububio@yahoo.com.br>
2022-08-22 10:08:47 -0300

    Adicionando código JS para fazer o palíndromo aparecer na tela

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function verificaPalindromo(string){
-    if(!string) return "String inexistente";
+let textoDigitado = document.getElementById("textoDigitado").value;
 
-    for(let i = 0; i < string.length / 2; i++){ //divido por 2 para verificar 2 caracteres ao mesmo tempo
-        if(string[i] !== string[string.length - 1 -i]){
-            var texto = "Não é um palíndromo"
-            return false;
-            
+function verificaPalindromo(textoDigitado){
+   
+    let resultado= " ";
+    if(!textoDigitado){
+        resultado = "Digite alguma palavra";
+        imprimeResultado(resultado);
+    } else{
+        for(let i = 0; i < textoDigitado.length / 2; i++){ //divido por 2 para verificar 2 caracteres ao mesmo tempo
+            if(textoDigitado[i] !== textoDigitado[textoDigitado.length - 1 -i]){
+                var texto = "Não é um palíndromo"
+                imprimeResultado(texto);
+            }else{
+                resultado = "É um palindrmo"
+                imprimeResultado(resultado);
+            }
         }
     }
-    return true;
 }
+function imprimeResultado(texto){
+    document.getElementById('resultado').innerHTML= texto;
+}
+
 console.log(verificaPalindromo("abba"));
 console.log(verificaPalindromo("ola"));
